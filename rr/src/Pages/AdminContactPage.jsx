2ef1f50f099dd23b82a84f48790c429a9df27abc
Manikanta222003 <manikantakedarisetti3@gmail.com>
2026-01-06 08:51:15 +0530

    Update contact API endpoint to production URL

--- rr/src/Pages/AdminContactPage.jsx
+++ rr/src/Pages/AdminContactPage.jsx
@@ -8,7 +8,7 @@ function AdminContactPage() {
   // 🔹 LOAD CONTACT MESSAGES
   const loadContacts = async () => {
     try {
-      const res = await fetch("http://localhost:5000/contact", {
+      const res = await fetch(" https://rr3-1-wo2n.onrender.com/contact", {
         credentials: "include", // admin auth cookie
       });
       const data = await res.json();
