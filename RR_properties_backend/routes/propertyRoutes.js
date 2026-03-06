016180768cc8ab7fec1e72a1bd468cae12861d8b
Manikanta222003 <manikantakedarisetti3@gmail.com>
2026-03-06 23:11:12 +0530

    Uncomment property route handlers for CRUD operations

--- RR_properties_backend/routes/propertyRoutes.js
+++ RR_properties_backend/routes/propertyRoutes.js
@@ -1,3 +1,4 @@
+
 import express from "express";
 import {
   addProperty,
@@ -7,6 +8,9 @@ import {
   deleteProperty,
   getPropertyFilters,
   getPropertyBannerImages,
+  getPropertyById,
+  updateProperty,
+  deletePropertyImage
 } from "../controllers/propertyController.js";
 
 import adminAuth from "../middlewares/authMiddleware.js";
@@ -24,6 +28,11 @@ router.post("/add", adminAuth, addProperty);
 router.get("/", getProperties);
 
 /* ==============================
+   GET SINGLE PROPERTY (FOR EDIT PAGE)
+============================== */
+router.get("/:id", getPropertyById);
+
+/* ==============================
    GET HOME PAGE PROPERTIES
 ============================== */
 router.get("/home", getHomeProperties);
@@ -39,6 +48,16 @@ router.get("/banners", getPropertyBannerImages);
 router.get("/filters", getPropertyFilters);
 
 /* ==============================
+   UPDATE PROPERTY (ADMIN EDIT)
+============================== */
+router.put("/update/:id", adminAuth, updateProperty);
+
+/* ==============================
+   DELETE SINGLE IMAGE FROM PROPERTY
+============================== */
+router.delete("/delete-image", adminAuth, deletePropertyImage);
+
+/* ==============================
    TOGGLE HOME PAGE VISIBILITY
 ============================== */
 router.patch("/toggle-home/:id", adminAuth, toggleHomeVisibility);
@@ -49,3 +68,4 @@ router.patch("/toggle-home/:id", adminAuth, toggleHomeVisibility);
 router.delete("/delete/:id", adminAuth, deleteProperty);
 
 export default router;
+
